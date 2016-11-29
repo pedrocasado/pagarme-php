3d91c27a411486a8c816870ff9cd97f1d94a13ff
Eduardo Nascimento <eduardo.nascimento@pagar.me>
2016-11-29 18:01:37 -0200

    Add tests for create subscription

---
+++ tests/acceptance/SubscriptionContext.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace PagarMe\Acceptance;
+
+use Behat\Behat\Context\Context;
+use Behat\Behat\Context\SnippetAcceptingContext;
+use Behat\Gherkin\Node\PyStringNode;
+use Behat\Gherkin\Node\TableNode;
+use Behat\Behat\Tester\Exception\PendingException;
+use Behat\Testwork\Hook\Scope\BeforeSuiteScope;
+use PagarMe\Sdk\SplitRule\SplitRuleCollection;
+use PagarMe\Sdk\Account\Account;
+use PagarMe\Sdk\Customer\Customer;
+use PagarMe\Sdk\Customer\Address;
+use PagarMe\Sdk\Customer\Phone;
+
+class SubscriptionContext extends BasicContext
+{
+    private $customer;
+    private $plan;
+    private $creditCard;
+
+    /**
+     * @Given a valid customer
+     */
+    public function aValidCustomer()
+    {
+        $this->customer = self::getPagarMe()
+            ->customer()
+            ->create(
+                'John Doe',
+                'john@test.com',
+                '25123317171',
+                new Address('Rua Teste', 123, 'Centro', '01034020'),
+                new Phone('11', '44445555')
+            );
+    }
+
+    /**
+     * @Given a valid plan
+     */
+    public function aValidPlan()
+    {
+        $this->plan = self::getPagarMe()
+            ->plan()
+            ->create(555, 30, 'Test Plan');
+    }
+
+    /**
+     * @Given a valid card
+     */
+    public function aValidCard()
+    {
+        $this->creditCard = self::getPagarMe()
+            ->card()
+            ->create('4539706041746367', "John Doe", '0725');
+    }
+
+    /**
+     * @When make a credit card subscription
+     */
+    public function makeACreditCardSubscription()
+    {
+        $this->subscription = self::getPagarMe()
+            ->subscription()
+            ->createCardSubscription(
+                $this->plan,
+                $this->creditCard,
+                $this->customer
+            );
+    }
+
+
+    /**
+     * @When make a boleto subscription
+     */
+    public function makeABoletoSubscription()
+    {
+        $this->subscription = self::getPagarMe()
+            ->subscription()
+            ->createBoletoSubscription(
+                $this->plan,
+                $this->customer
+            );
+    }
+
+    /**
+     * @Then a subscription must be created
+     */
+    public function aSubscriptionMustBeCreated()
+    {
+        assertInstanceOf(
+            'PagarMe\Sdk\Subscription\Subscription',
+            $this->subscription
+        );
+    }
+
+     /**
+     * @Then the payment method must be :paymentMethod
+     */
+    public function thePaymentMethodMustBe($paymentMethod)
+    {
+        assertEquals($paymentMethod, $this->subscription->getPaymentMethod());
+    }
+}
---
+++ tests/acceptance/features/subscription.feature
@@ -0,0 +1,19 @@
+Feature: Subscription
+ Como cliente da Pagar.me integrando uma aplicação PHP
+ Eu quero uma camada de abstração
+ Para que eu possa realizar assinaturas
+
+  Scenario: Create a subscription
+    Given a valid customer
+    And a valid plan
+    And a valid card
+    When make a credit card subscription
+    Then a subscription must be created
+    And the payment method must be 'credit_card'
+
+  Scenario: Create a boleto subscription
+    Given a valid customer
+    And a valid plan
+    When make a boleto subscription
+    Then a subscription must be created
+    And the payment method must be 'boleto'
